64e5deaaf62d928b31a4f9c77a0a942b10684610
Muhammad Fajar <mf4997537@gmail.com>
2026-06-07 18:41:24 +0700

    Move package.json

--- cafe-app/backend/config/db.js
+++ cafe-app/backend/config/db.js
@@ -1,18 +1,19 @@
-import mysql from "mysql2/promise"; // Tambahkan /promise di sini
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "", // Sesuaikan password database Anda
-  database: "cafe_db", // Ganti dengan nama database Anda
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306, // Menambahkan port dinamis dari Railway
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
 // Cek koneksi
-console.log("Database Pool initialized...");
+console.log("Database Pool initialized to Railway Cloud...");
 
 export default db;
