7bd09de6d2590edafce5994f149eee1a7c0ac825
Muhammad Fajar <mf4997537@gmail.com>
2026-06-07 16:29:10 +0700

    Fix: Perbaikan route css

--- cafe-app/src/pages/HandleOrder.jsx
+++ cafe-app/src/pages/HandleOrder.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import api from "../services/api";
-import "../handleOrders.css";
+import "../HandleOrders.css";
 
 export default function HandleOrders() {
   const [orders, setOrders] = useState([]);
