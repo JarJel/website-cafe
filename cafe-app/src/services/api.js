2a549ba58fd82317082cf96b2177e57a4c05dde8
Muhammad Fajar <mf4997537@gmail.com>
2026-06-07 19:15:00 +0700

    Fix: ubah baseURL axios ke domain backend cloud

--- cafe-app/src/services/api.js
+++ cafe-app/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "mysql-production-0700.up.railway.app",
+  timeout: 10000,
+  headers: {
+    "Content-Type" : "application/json",
+  }
 });
 
 export default api;
